b9702c3c4966613a284ecc73534eeae333c7b753
ematrix25 <ematrix25@gmail.com>
2014-08-17 16:43:00 -0300

    Modificacoes no ArqEntrada
    Tratando as excecoes

--- src/br/com/ufs/mirrorFTP/Teste.java
+++ src/br/com/ufs/mirrorFTP/Teste.java
@@ -1,9 +1,15 @@
 package br.com.ufs.mirrorFTP;
 
+import br.com.ufs.mirrorFTP.local.ArqEntrada;
+import br.com.ufs.mirrorFTP.servidor.FTP;
+
 public class Teste {
-
+	
+	ArqEntrada entrada = new ArqEntrada();
+	FTP ftp = new FTP();
+	
 	public static void main(String[] args) {
-
+		
 	}
 
 }
--- src/br/com/ufs/mirrorFTP/local/ArqEntrada.java
+++ src/br/com/ufs/mirrorFTP/local/ArqEntrada.java
@@ -3,30 +3,38 @@ package br.com.ufs.mirrorFTP.local;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class ArqEntrada {
-	//AREA DE VARIAVEIS GLOBAIS
 	private String host, usuario, senha, dirLocal, dirRemoto;
 	private int porta, intervalo;
 	
-	public ArqEntrada() throws IOException{
+	public ArqEntrada() {
 		this.read();
 	}
-	void read() throws IOException {
+	void read() {
 		File f = new File("entradas.txt");
-		InputStream is = new FileInputStream(f);
-		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-		host = br.readLine();
-		porta = new Integer(br.readLine());
-		intervalo = new Integer(br.readLine());
-		usuario = br.readLine();
-		senha = br.readLine();
-		dirLocal = br.readLine();
-		dirRemoto = br.readLine();
-		br.close();
+		InputStream is;
+		BufferedReader br;
+		try {
+			is = new FileInputStream(f);
+			br = new BufferedReader(new InputStreamReader(is));
+			host = br.readLine();
+			porta = new Integer(br.readLine());
+			intervalo = new Integer(br.readLine());
+			usuario = br.readLine();
+			senha = br.readLine();
+			dirLocal = br.readLine();
+			dirRemoto = br.readLine();
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public String getHost() {
