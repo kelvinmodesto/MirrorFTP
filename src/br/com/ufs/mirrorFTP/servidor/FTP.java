b27c05f287d70241012b8322837c324881c01bb8
ematrix25 <ematrix25@gmail.com>
2014-08-17 14:54:04 -0300

    Alterando o FTP
    Resumi e adicionei metodos que estavam faltando

--- src/br/com/ufs/mirrorFTP/servidor/FTP.java
+++ src/br/com/ufs/mirrorFTP/servidor/FTP.java
@@ -2,6 +2,7 @@ package br.com.ufs.mirrorFTP.servidor;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -10,6 +11,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Date;
 import java.util.StringTokenizer;
 
 public class FTP {
@@ -17,7 +19,8 @@ public class FTP {
 	private InputStream isContr;
 	private OutputStream osContr;
 	private Socket dados;
-	private InputStream isData;
+	private InputStream isDados;
+	private OutputStream osDados;
 
 	private String enviarCmd() {
 		String resp = null;
@@ -76,8 +79,8 @@ public class FTP {
 		this.enviarCmd("RMD " + pasta + "\r\n");
 	}
 
-	public void mudarDir(String caminho) {
-		this.enviarCmd("CWD " + caminho + "\r\n");
+	public void mudarDir(String diretorio) {
+		this.enviarCmd("CWD " + diretorio + "\r\n");
 	}
 
 	public void mostrarDirAtual() {
@@ -95,7 +98,7 @@ public class FTP {
 		int port = value1 * 256 + value2;
 		try {
 			this.dados = new Socket(ip, port);
-			this.isData = dados.getInputStream();
+			this.isDados = dados.getInputStream();
 			dados.getOutputStream();
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
@@ -104,14 +107,14 @@ public class FTP {
 		}
 	}
 
-	public String list(String caminho) {
+	public String list(String diretorio) {
 		this.entrarNoModoPASV();
-		this.enviarCmd("LIST " + caminho + "\r\n");
+		this.enviarCmd("LIST " + diretorio + "\r\n");
 		BufferedReader br;
 		String resp = "";
 		String line;
 		try {
-			br = new BufferedReader(new InputStreamReader(isData));
+			br = new BufferedReader(new InputStreamReader(isDados));
 			while ((line = br.readLine()) != null) {
 				resp = resp + "\n" + line;
 			}
@@ -122,20 +125,26 @@ public class FTP {
 		return resp;
 	}
 
-	public void baixarArquivo(String caminho,String arquivo) {
+	private void mudarTipo(String tipo) {
+		this.enviarCmd("TYPE " + tipo + "\r\n");
+	}
+
+	public void baixarArquivo(String diretorio, String arquivo) {
 		this.entrarNoModoPASV();
+		this.mudarTipo("I");
 		this.enviarCmd("RETR " + arquivo + "\r\n");
-		File file = new File(caminho + arquivo);
+		File file = new File(diretorio + arquivo);
 		FileOutputStream fos = null;
 		try {
 			fos = new FileOutputStream(file);
 			byte[] buf = new byte[1000];
 			int len;
-			while ((len = this.isData.read(buf)) != -1) {
+			while ((len = this.isDados.read(buf)) != -1) {
 				fos.write(buf, 0, len);
 			}
 			fos.flush();
 			fos.close();
+			dados.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -144,11 +153,38 @@ public class FTP {
 		this.enviarCmd();
 	}
 
-	@SuppressWarnings("unused")
-	private void mudarTipo(String type) throws IOException {
-		String msg = "TYPE " + type + "\r\n";
-		this.osContr.write(msg.getBytes());
+	public void enviarArquivo(String diretorio, String arquivo) {
+		this.entrarNoModoPASV();
+		this.mudarTipo("I");
+		this.enviarCmd("STOR " + arquivo + "\r\n");
+		File file = new File(diretorio + arquivo);
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(file);
+			byte[] buf = new byte[1000];
+			int len;
+			while ((len = fis.read(buf)) != -1) {
+				this.osDados.write(buf, 0, len);
+			}
+			fis.close();
+			dados.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		this.enviarCmd();
+	}
+
+	public Date getDataModArq(String arquivo) {
+		String resp = this.enviarCmd("MDTM " + arquivo + "\r\n").replaceAll(
+				" ", "");
+		char[] aux = resp.toCharArray();
+		for (int i = 0; i < 3; i++) {
+			aux[i] = '0';
+		}
+		resp = new String(aux);
+		return new Date(Long.parseLong(resp));
 	}
 
 }
