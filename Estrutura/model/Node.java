40f19c1f5697d22fd0591ee7e7192499d2d69e22
kelvinmodesto <km.modesto@gmail.com>
2014-08-25 17:34:08 -0300

    ajuste

--- Estrutura/model/Node.java
+++ Estrutura/model/Node.java
@@ -1,5 +1,11 @@
 package model;
 
-public class Node {
+import java.sql.Date;
 
+public abstract class Node {
+	private String nome;
+	private Date data;
+	public Node(String nome, Date date){
+		
+	}
 }
