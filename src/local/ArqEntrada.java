e55b8bff4c3f4fedd6331f82c9f3b0f93e6e2b15
ematrix25 <ematrix25@gmail.com>
2014-08-29 22:51:59 -0300

    Gerando ArqEntrada caso nao exista

--- src/local/ArqEntrada.java
+++ src/local/ArqEntrada.java
@@ -1,27 +1,45 @@
 package local;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+import javax.swing.JOptionPane;
 
 public class ArqEntrada {
+	private File arq;
 	private String host, usuario, senha, dirLocal, dirRemoto;
 	private int porta, intervalo;
 
 	public ArqEntrada() {
-		this.read();
+		arq = new File("entradas.txt");
+		gerarArquivo(arq);
+		this.lerArq();
 	}
-
-	private void read() {
-		File f = new File("entradas.txt");
+	
+	private void gerarArquivo(File arq) {
+		if(!arq.exists())
+			try {
+				arq.createNewFile();
+				escreverArq();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}		
+	}
+	
+	private void lerArq() {
 		InputStream is;
 		BufferedReader br;
 		try {
-			is = new FileInputStream(f);
+			is = new FileInputStream(arq);
 			br = new BufferedReader(new InputStreamReader(is));
 			host = br.readLine();
 			porta = new Integer(br.readLine());
@@ -31,6 +49,30 @@ public class ArqEntrada {
 			dirLocal = br.readLine();
 			dirRemoto = br.readLine();
 			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void escreverArq() {
+		OutputStream os;
+		BufferedWriter bw;
+		try {
+			os = new FileOutputStream(arq);
+			bw = new BufferedWriter(new OutputStreamWriter(os));
+			bw.write("ftp.xpg.com.br\n");
+			bw.write("21\n");
+			bw.write("60\n");
+			usuario = JOptionPane.showInputDialog("Digite o usuario");
+			bw.write(usuario+"\n");
+			senha = JOptionPane.showInputDialog("Digite a senha");
+			bw.write(senha+"\n");
+			dirLocal = JOptionPane.showInputDialog("Digite o diretorio local");
+			bw.write(dirLocal+"\n");
+			bw.write("/");
+			bw.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
